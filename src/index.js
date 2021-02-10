766f187b1a9238e000806ba6df98aeb8fdbdb7bc
Dmitry <47834553+Belastotski@users.noreply.github.com>
2021-02-10 01:00:33 +0100

    feat: add functional check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+          let arr = []
+        Array.from(str).forEach(val => {
+            let br = bracketsConfig.find(arr => arr[0] == val || arr[1] == val)
+                br[1] == val && arr.length && arr[arr.length-1] == br[0]? arr.pop() : arr.push(val)
+        })
+        return !arr.length
 }
